675dedac6a73a67818dc99bfab68baf3f54005c3
Dhaval <dhaval2025@gmail.com>
2016-10-21 14:12:55 -0700

    Update run_buck_cache_client.sh

--- run_buck_cache_client.sh
+++ run_buck_cache_client.sh
@@ -2,4 +2,4 @@
 
 ./gradlew distJar
 
-$JAVA_HOME/bin/java -Xmx8G  -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:MaxDirectMemorySize=6g -Dlog.home=/var/log/buck-cache-client/logs -jar cache/build/libs/cache-1.0.0-standalone.jar server cache/src/dist/config/$1.yml
+$JAVA_HOME/bin/java -Xmx4G  -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:MaxDirectMemorySize=6g -Dlog.home=/var/log/buck-cache-client/logs -jar cache/build/libs/cache-1.0.0-standalone.jar server cache/src/dist/config/$1.yml
